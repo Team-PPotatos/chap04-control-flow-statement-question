b7c1a25cc03bc24ecb21fb13339e52d1b194c66f
DESKTOP-KTEEVGU\hanme <2hanme527@naver.com>
2023-05-05 01:54:38 +0900

    [section2 normal 3] 수박수박수
    1부터 입력받은 정수까지 홀수이면 "수", 짝수이면 "박"이 정수만큼 누적되어 출력된다.

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application3.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application3.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section02.looping_and_branching.level02.normal;
 
+import java.util.Scanner;
+
 public class Application3 {
 
 	public static void main(String[] args) {
@@ -12,7 +14,18 @@ public class Application3 {
 		 * -- 출력 예시 --
 		 * 수박수박수
 		 * */
-		
-	}
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("정수를 입력하세요 : ");
+		int num = sc.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = 1; i < num + 1; i++){
+			if (i % 2 == 1) sb.append("수");	// 홀수면 "수" 추가
+			else sb.append("박");				// 짝수면 "박" 추가
+		}
+
+		System.out.println(sb);
+	}
 }
