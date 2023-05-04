b554fe49cba24c07efb08b5075271b8cfbc033a9
DESKTOP-KTEEVGU\hanme <2hanme527@naver.com>
2023-05-05 01:56:27 +0900

    [section2 hard 1] 소수 판별
    입력받은 2보다 큰 정수에 대해 소수 판별한 결과를 출력한다.
    2보다 큰 정수가 아닌 경우, 경고 문구를 출력하고 다시 입력하게 한다.

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level03/hard/Application1.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level03/hard/Application1.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.section02.looping_and_branching.level03.hard;
 
+import java.util.Scanner;
+
 public class Application1 {
 
 	public static void main(String[] args) {
-		
+
 		/* 2보다 큰 정수를 하나 입력 받아 그 수가 소수인지 아닌지를 판별해서 출력하세요
 		 * 소수인 경우 "소수다." 출력, 소수가 아닌 경우 "소수가 아니다." 출력
 		 * 
@@ -20,7 +22,29 @@ public class Application1 {
 		 * -- 출력 예시 --
 		 * 소수다.
 		 * */
-		
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+
+		System.out.print("2보다 큰 정수를 하나 입력하세요 : ");
+
+		/* 3 이상의 값을 받기 전에는 계속 입력을 받게 한다. */
+		while (num < 3) {
+			num = sc.nextInt();
+			if (num < 3) System.out.print("잘못 입력하셨습니다. 다시 입력하세요 : ");
+		}
+
+		/* 소수가 아닌게 판명되면 프로그램 종료 */
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				System.out.println("소수가 아니다.");
+				return;
+			}
+		}
+
+		/* 반복문을 돌아도 나눠지는 수가 없었다. */
+		System.out.println("소수다.");
+
+
 	}
 
 }
