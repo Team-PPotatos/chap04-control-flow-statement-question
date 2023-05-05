c4e4499bbc1efe08300395a2b94cb1a965811373
JUNE-LABTOP\김준열 <june5228@gmail.com>
2023-05-05 23:10:30 +0900

    [fix] section2.looping_and_branching.level2.normal.app2

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application2.java
@@ -1,12 +1,10 @@
 package com.ohgiraffers.section02.looping_and_branching.level02.normal;
 
-import java.util.Scanner;
 
 public class Application2 {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
 		String result = "";
 		for(char i = 'a'; i <= 'z'; i++){
 			result += i;
