da670a5ae6a1a1840bb262779f97c1768a0e39a1
jinchiim <nellroll@naver.com>
2023-05-05 19:23:46 +0900

    section02 level02.normal Application2 init-commit
    level 02 Application2 init-commit a-z 개행없이

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application2.java
@@ -3,13 +3,17 @@ package com.ohgiraffers.section02.looping_and_branching.level02.normal;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
+		for (char i='a'; i <='z'; i++){
+			System.out.print(i);		// 개행없이 입력 위해 print
+		}
+
 		/* 반복문을 이용하여 알파벳 소문자 'a'부터 'z'까지를 개행 없이 차례로 출력하세요
 		 * 
 		 * -- 출력 예시 --
 		 * abcdefghijklmnopqrstuvwxyz
 		 * */
-		
+
 	}
 
 }
