2fec1bc99691fc353bb9b95d379b71c470740f09
jinchiim <nellroll@naver.com>
2023-05-05 19:24:14 +0900

    section02 level03.normal Application3 init-commit
    level 02 Application3 init-commit 박수

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application3.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application3.java
@@ -1,8 +1,22 @@
 package com.ohgiraffers.section02.looping_and_branching.level02.normal;
 
+import java.util.Scanner;
+
 public class Application3 {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수를 입력하세요 : ");
+		int num = sc.nextInt();
+
+		for (int i=1; i <= num; i++){
+			if (i%2==0){				// 짝수 일 경우 계산
+				System.out.print("박");
+			} else {
+				System.out.print("수");
+			}
+		}
 		/* 정수를 입력받아 1부터 입력받은 정수까지
 		 * 홀수이면 "수", 짝수이면"박"이 정수만큼 누적되어 출력되게 작성하시오.
 		 * 
@@ -12,7 +26,7 @@ public class Application3 {
 		 * -- 출력 예시 --
 		 * 수박수박수
 		 * */
-		
+
 	}
 
 }
