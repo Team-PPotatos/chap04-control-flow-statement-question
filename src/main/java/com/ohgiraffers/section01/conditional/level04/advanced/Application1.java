5eddc4a3f06b353b029d9a02ab0aa76105ec0864
jinchiim <nellroll@naver.com>
2023-05-05 18:57:22 +0900

    section01 level04.advanced Application1 init-commit
    level 04 Application1 init-commit 과목 점수

--- src/main/java/com/ohgiraffers/section01/conditional/level04/advanced/Application1.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level04/advanced/Application1.java
@@ -1,10 +1,46 @@
 package com.ohgiraffers.section01.conditional.level04.advanced;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Application1 {
 
 	public static void main(String[] args) {
-		
-		/* 국어, 영어, 수학 점수를 입력받아 
+
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> subjectList = new ArrayList<String>();	// 가상의 array 생성
+
+		System.out.print("국어 점수를 입력하세요 : ");
+		int korean = sc.nextInt();
+		System.out.print("영어 점수를 입력하세요 : ");
+		int english = sc.nextInt();
+		System.out.print("수학 점수를 입력하세요: ");
+		int math = sc.nextInt();
+
+		int average = (korean + english + math) / 3;
+
+		if (average >= 60 && korean >= 40 && english >= 40 && math >= 40) {
+			System.out.println("합격입니다!");
+		} else {
+			if (korean < 40) {
+				subjectList.add("국어");								// 해당 과목에 문제가 있을 경우 add
+			}
+			if (english < 40) {
+				subjectList.add("영어");
+			}
+			if (math < 40) {
+				subjectList.add("수학");
+			}
+			if (average < 60) {
+				subjectList.add("평균");
+			}
+		}
+
+		for (String s : subjectList){
+			System.out.println(s+ " 점수 미달로 불합격 입니다.");		// array 안에 있는걸 하나씩 get
+		}
+
+		/* 국어, 영어, 수학 점수를 입력받아
 		 * 평균 점수가 60점 이상이면서 각 과목이 40점 이상인 조건이 만족하면 "합격입니다!" 를 출력하세요,
 		 * 단, 합격이 아닌 경우 불합격 사유가 무엇인지를 모두 출력해주어야 합니다.
 		 * 평균점수 미달인 경우 "평균점수 미달로 불합격입니다." 라고 출력하고,
@@ -20,7 +56,7 @@ public class Application1 {
 		 * 영어 점수 미달로 불합격입니다.
 		 * 수학 점수 미달로 불합격입니다.
 		 * */
-		
+
 	}
 
 }
