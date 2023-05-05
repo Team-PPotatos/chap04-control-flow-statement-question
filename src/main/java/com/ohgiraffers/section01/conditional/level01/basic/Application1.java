35d9742004709e586b256b7d20e18aeb91f5056e
jinchiim <nellroll@naver.com>
2023-05-05 20:05:22 +0900

    [fix] section01 level01.basic Application1
    level 01 Application1 PR 정리 중 문제 발견, 수정

--- src/main/java/com/ohgiraffers/section01/conditional/level01/basic/Application1.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level01/basic/Application1.java
@@ -8,7 +8,7 @@ public class Application1 {
 
 		System.out.print("정수를 하나 입력하세요 : ");
 		int a = sc.nextInt();
-		System.out.println(a>=0?"양수다":"양수가 아니다."); // a가 0보다 크거나 같은경우 양수
+		System.out.println(a>0?"양수다":"양수가 아니다."); // a가 0보다 크거나 같은경우 양수
 
 		/* 정수를 하나 입력 받아 그 수가 양수이면 "양수다." 라고 출력하고,
 		 * 양수가 아닌 경우 "양수가 아니다." 라고 출력하세요
